eade51b030d06af0e4da22e359cac5d201d08eec
Ryne Fagin <rfagin@redhat.com>
2017-09-05 21:53:45 -0700

    init commit

---
+++ src/main/java/org/ghc/osd/java/springboot/servlet_service/servlet/EntryController.java
@@ -0,0 +1,17 @@
+package org.ghc.osd.java.springboot.servlet_service.servlet;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class EntryController {
+
+    @RequestMapping("/entry")
+    public String entry(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "entry";
+    }
+
+}
